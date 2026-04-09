5016abb6a62fbd6cedf8493e12fb792703060000
NotAlwaysGreen <neuvek.zelen@gmail.com>
2026-04-09 21:23:29 +0200

    Pause added
    added pause scene and main menu, but something needs to be done to connect main menu thing

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject Container;
+     void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Container.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+    }
+
+    public void Resume()
+    {
+        Container.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    
+    public void MainMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1f;
+    }
+
+    public void Reset()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("GameScene");
+        Time.timeScale = 1f;
+
+    }
+}
